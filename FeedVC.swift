0cf0752b6f23d2f293b3b13b2f55388a9e02bdf9
mogalia786 <imac@Heros-iMac.local>
2016-10-19 01:14:27 +0200

    TableUI Setup

--- FeedVC.swift
+++ FeedVC.swift
@@ -10,15 +10,31 @@ import UIKit
 import SwiftKeychainWrapper
 import Firebase
 
-class FeedVC: UIViewController {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate=self
+        tableView.dataSource=self
     }
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "postCell") as! postCell    }
   
+    
+    
+    
     @IBAction func btnLogOut(_ sender: AnyObject) {
         KeychainWrapper.standard.removeObject(forKey: KEY_UID)
         try! FIRAuth.auth()?.signOut()
---
+++ postCell.swift
@@ -0,0 +1,25 @@
+//
+//  postCell.swift
+//  SocialMedia
+//
+//  Created by ebrahim on 10/18/16.
+//  Copyright © 2016 iSTROBE. All rights reserved.
+//
+
+import UIKit
+
+class postCell: UITableViewCell {
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var caption: UITextView!
+    @IBOutlet weak var likeLbl: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+  
+}
