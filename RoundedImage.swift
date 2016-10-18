0e0e3c92a324e1398ac71008630c9b2182c248ca
mogalia786 <imac@Heros-iMac.local>
2016-10-18 21:28:54 +0200

    UI

--- RoundedImage.swift
+++ RoundedImage.swift
@@ -1,21 +1,27 @@
 //
-//  FancyRoundedButton.swift
+//  RoundedImage.swift
 //  SocialMedia
 //
-//  Created by ebrahim on 10/14/16.
+//  Created by ebrahim on 10/15/16.
 //  Copyright © 2016 iSTROBE. All rights reserved.
 //
 
 import UIKit
 
-class FancyRoundedButton: UIButton {
+class RoundedImage: UIImageView {
     override func awakeFromNib() {
         super.awakeFromNib()
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         layer.shadowOpacity = 0.8
+
+        
     }
-    
-   
+    override func layoutSubviews(){
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width/2
+        
+    }
+
 }
